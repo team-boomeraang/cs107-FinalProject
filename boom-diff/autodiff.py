9620254acdc808f1d1c27cf7676965d4b5cc95d0
minhuanli <minhuanli@g.harvard.edu>
2020-11-09 12:59:55 -0500

    Version0, MH, Add placeholder TODOs for primary functions

--- boom-diff/autodiff.py
+++ boom-diff/autodiff.py
@@ -30,6 +30,56 @@ class AD():
         # Will assume form of x_1, ..., x_n
         self.partial_dict = {name: 1}
 
+    def set_params(self, params):
+	    pass # TODO
+
+    def __add__(self, other):
+	    pass # TODO
+    
+    def __radd__(self, other):
+	    pass # TODO
+    
+    def __sub__(self, other):
+	    pass # TODO
+    
+    def __rsub__(self, other):
+	    pass # TODO
+    
+    def __mul__(self, other):
+	    pass # TODO
+    
+    def __rmul__(self, other):
+	    pass # TODO
+
+    def __truediv__(self, other):
+	    pass # TODO
+
+    def __rtruediv__(self, other):
+	    pass # TODO 
+
+    def __pow__(self, other):
+	    pass # TODO
+
+    def __rpow__(self, other):
+	    pass # TODO
+
+    @staticmethod
+    def sin(x):
+	    pass # TODO
+
+    @staticmethod
+    def cos(x):
+	    pass # TODO
+
+    @staticmethod
+    def tan(x):
+	    pass # TODO
+
+    @staticmethod
+    def log(x):
+	    pass # TODO
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
