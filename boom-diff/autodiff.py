841ea80c6cfbd3249af0c25b4994aee0fedb6844
minhuanli <minhuanli@g.harvard.edu>
2020-11-09 21:22:27 -0500

    Version0, MH, change the indention to 4 spaces

--- boom-diff/autodiff.py
+++ boom-diff/autodiff.py
@@ -1,24 +1,27 @@
 import numpy as np
 
 class AD():
-    
-    def __init__(self, eval_pt, name='x1'):
+
+    def __init__(self, eval_pt, der_dict={'x1':1}):
         """Initializes class structure
 
         Parameters
         ----------
         eval_pt : float
-            Point for evaluation of value and derivative of AD class
-
+            Value of the current function/variable
+        der_dict : dict
+            derivative value dictionary of all variables
+        
         Returns
         -------
         None.
         
         Examples
         --------
-        >>> AD(3.6).func_val
+        >>> x1 = AD(3.6, {'x1':1})
+        >>> x1.func_val
         3.6
-        >>> AD(3.6).partial_dict
+        >>> x1.partial_dict
         {'x1': 1}
         """
         
@@ -28,56 +31,56 @@ class AD():
         
         # Set partial derivative dictionary
         # Will assume form of x_1, ..., x_n
-        self.partial_dict = {name: 1}
-
+        self.partial_dict = der_dict
+        
     def set_params(self, params):
-	    pass # TODO
+        pass # TODO
 
     def __add__(self, other):
-	    pass # TODO
-    
+        pass # TODO
+
     def __radd__(self, other):
-	    pass # TODO
-    
+        pass # TODO
+
     def __sub__(self, other):
-	    pass # TODO
-    
+        pass # TODO
+
     def __rsub__(self, other):
-	    pass # TODO
-    
+        pass # TODO
+
     def __mul__(self, other):
-	    pass # TODO
-    
+        pass # TODO
+
     def __rmul__(self, other):
-	    pass # TODO
+        pass # TODO
 
     def __truediv__(self, other):
-	    pass # TODO
+        pass # TODO
 
     def __rtruediv__(self, other):
-	    pass # TODO 
+        pass # TODO 
 
     def __pow__(self, other):
-	    pass # TODO
+        pass # TODO
 
     def __rpow__(self, other):
-	    pass # TODO
+        pass # TODO
 
     @staticmethod
     def sin(x):
-	    pass # TODO
+        pass # TODO
 
     @staticmethod
     def cos(x):
-	    pass # TODO
+        pass # TODO
 
     @staticmethod
     def tan(x):
-	    pass # TODO
+        pass # TODO
 
     @staticmethod
     def log(x):
-	    pass # TODO
+        pass # TODO
 
 
 if __name__ == '__main__':
