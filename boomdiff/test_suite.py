3ff03bc3008d08cc7734754e5a934bdcf1baab2d
oksana-makarova <oksana_makarova@g.harvard.edu>
2020-11-17 19:15:06 -0500

    Test suite with 87% coverage

--- boomdiff/test_suite.py
+++ boomdiff/test_suite.py
@@ -5,49 +5,77 @@ import numpy as np
 
 #creating templates that will be used to test the functions
 @pytest.fixture
-def basic_cls():
+def trig_cls():
     x = boomdiff.AD(np.pi/4, {'x1': 1.})
     return x
 
 @pytest.fixture
-def basic_var():
+def trig_var():
     x = np.pi/4
     return x
 
-#testing basic functions
-def test_init(basic_cls):
-    assert basic_cls.func_val == np.pi/4
-    assert basic_cls.partial_dict['x1'] == 1
-    assert len(basic_cls.partial_dict.keys()) == 1
+@pytest.fixture
+def basic_cls():
+    x = boomdiff.AD(2, {'x1': 1.})
+    return x
 
-def test_sin(basic_cls, basic_var):
-    f1 = boomdiff.AD.sin(basic_cls)
-    f2 = boomdiff.AD.sin(basic_var)
+@pytest.fixture
+def basic_var():
+    x = 0.5
+    return x
+
+#testing basic functions
+def test_init(trig_cls):
+    assert trig_cls.func_val == np.pi/4
+    assert trig_cls.partial_dict['x1'] == 1
+    assert len(trig_cls.partial_dict.keys()) == 1
+
+def test_sin(trig_cls, trig_var):
+    f1 = boomdiff.AD.sin(trig_cls)
+    f2 = boomdiff.AD.sin(trig_var)
 
     assert f1.func_val == np.sin(np.pi/4)
     assert f1.partial_dict['x1'] == np.cos(np.pi/4)
     assert f2 == np.sin(np.pi/4)
 
-def test_cos(basic_cls, basic_var):
-    f1 = boomdiff.AD.cos(basic_cls)
-    f2 = boomdiff.AD.cos(basic_var)
+def test_cos(trig_cls, trig_var):
+    f1 = boomdiff.AD.cos(trig_cls)
+    f2 = boomdiff.AD.cos(trig_var)
 
     assert f1.func_val == np.cos(np.pi/4)
     assert f1.partial_dict['x1'] == -np.sin(np.pi/4)
     assert f2 == np.cos(np.pi/4)
 
-def test_tan(basic_cls, basic_var):
-    f1 = boomdiff.AD.tan(basic_cls)
-    f2 = boomdiff.AD.tan(basic_var)
+def test_tan(trig_cls, trig_var):
+    f1 = boomdiff.AD.tan(trig_cls)
+    f2 = boomdiff.AD.tan(trig_var)
 
     assert f1.func_val == np.tan(np.pi/4)
     assert f1.partial_dict['x1'] == 1/(np.cos(np.pi/4)**2)
     assert f2 == np.tan(np.pi/4)
 
-def test_log(basic_cls, basic_var):
-    f1 = boomdiff.AD.log(basic_cls)
-    f2 = boomdiff.AD.log(basic_var)
+def test_log(trig_cls, trig_var):
+    f1 = boomdiff.AD.log(trig_cls)
+    f2 = boomdiff.AD.log(trig_var)
 
     assert f1.func_val == np.log(np.pi/4)
     assert f1.partial_dict['x1'] == 1/(np.pi/4)
     assert f2 == np.log(np.pi/4)
+
+def test_ops(basic_cls, basic_var):
+    assert (basic_cls + basic_var).func_val == 2.5
+    assert (basic_var + basic_cls).func_val == 2.5
+    assert (basic_cls + basic_cls).func_val == 4
+    #assert (basic_cls + basic_cls).func_val == 2.5
+
+    assert (basic_cls - basic_var).func_val == 1.5
+    assert (basic_var - basic_cls).func_val == -1.5
+    assert (basic_cls - basic_cls).func_val == 0
+
+    assert (basic_cls * basic_var).func_val == 1
+    assert (basic_var * basic_cls).func_val == 1
+    assert (basic_cls * basic_cls).func_val == 4
+
+    assert (basic_cls / basic_var).func_val == 4
+    assert (basic_var / basic_cls).func_val == 0.25
+    assert (basic_cls / basic_cls).func_val == 1
